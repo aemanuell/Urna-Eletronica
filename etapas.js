d8f5b572674a22b07ae9da98d43edb83c3259c27
Antonio Emanuel <anttonioemanuell@gmail.com>
2021-09-24 20:50:54 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,47 @@
+let etapas = [{
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'TKT_0522.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'TKT_0127.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [{
+                numero: '99',
+                nome: ' Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'TKT_0077.jpg', legenda: 'Prefeito' },
+                    { url: 'TKT_0093.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: 'TKT_0178.jpg', legenda: 'Prefeito' },
+                    { url: 'TKT_0112.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    },
+]
